822d5aee96cc389b55cbb29c3cfe9385acc5d20e
Adam Slanina <slaninaa16@gmail.com>
2024-05-03 04:08:39 +0200

    Opakovani

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -1,7 +1,47 @@
 package org.example;
 
+import java.util.ArrayList;
+import java.util.Scanner;
+
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        ArrayList<Prodejci>list = new ArrayList<>();
+        Prodejci prodejce1 = new Prodejci("Tomáš","15. 5. 1984", 15,0.5,"Brno","8BR984",7.5,"192.168.48.39");
+        list.add(prodejce1);
+        System.out.println(prodejce1);
+        System.out.println(prodejce1.prumerNaSmlouvu());
+        System.out.println();
+        Scanner scanner = new Scanner(System.in);
+        while(true){
+            System.out.println("Pro vytvoření prodejce zadejte jméno, pro ukončení programu napiště stop");
+            String input = scanner.nextLine();
+            if(input.equals("stop")){
+                break;
+            }
+            System.out.println("Narození:");
+            String narozeni = scanner.nextLine();
+            System.out.println("Počet smluv: ");
+            String smlouvy = scanner.nextLine();
+            double smlouvyDouble = Double.parseDouble(smlouvy);
+            System.out.println("Počet tun: ");
+            String mrkev = scanner.nextLine();
+            double mrkevDouble = Double.parseDouble(mrkev);
+            System.out.println("Město: ");
+            String mesto = scanner.nextLine();
+            System.out.println("SPZ: ");
+            String spz = scanner.nextLine();
+            System.out.println("Spotřeba: ");
+            String spotreba = scanner.nextLine();
+            double spotrebaDouble = Double.parseDouble(spotreba);
+            System.out.println("IP: ");
+            String IP = scanner.nextLine();
+            list.add(new Prodejci(input,narozeni,smlouvyDouble,mrkevDouble,mesto,spz,spotrebaDouble,IP));
+        }
+
+        for (Prodejci prodejci : list) {
+            System.out.println(prodejci);
+            System.out.println(prodejci.prumerNaSmlouvu());
+            System.out.println("");
+        }
     }
 }
--- src/main/java/org/example/Prodejci.java
+++ src/main/java/org/example/Prodejci.java
@@ -1,2 +1,59 @@
-package org.example;public class Prodejci {
+package org.example;
+
+public class Prodejci {
+    private String jmeno;
+    private String narozeni;
+    private double smlouvy;
+    private double mrkev;
+    private String mesto;
+    private String spz;
+    private double spotreba;
+    private String IP;
+
+    public Prodejci(String jmeno, String narozeni, double smlouvy, double mrkev, String mesto, String spz, double spotreba, String IP){
+        this.jmeno=jmeno;
+        this.narozeni=narozeni;
+        this.smlouvy=smlouvy;
+        this.mrkev=mrkev;
+        this.mesto=mesto;
+        this.spz=spz;
+        this.spotreba=spotreba;
+        this.IP=IP;
+    }
+
+    public double getMrkev(){
+        return this.mrkev;
+    }
+    public double getSmlouvy(){
+        return this.smlouvy;
+    }
+
+    public String getJmeno(){
+        return this.jmeno;
+    }
+    public String getNarozeni(){
+        return this.narozeni;
+    }
+    public String getMesto(){
+        return this.mesto;
+    }
+    public String getSpz(){
+        return this.spz;
+    }
+    public String getIP(){
+        return this.IP;
+    }
+
+    public double getSpotreba() {
+        return this.spotreba;
+    }
+
+    public double prumerNaSmlouvu(){
+        return getMrkev()/getSmlouvy();
+    }
+
+    @Override
+    public String toString() {
+        return this.jmeno+"/"+this.narozeni+"/"+this.smlouvy+"/"+this.mrkev+"/"+this.mesto+"/"+this.spz+"/"+this.spotreba+"/"+this.IP;
+    }
 }
